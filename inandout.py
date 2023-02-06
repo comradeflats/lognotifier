8e47e60f23be7fb8ee4821391ae91c9431bb8641
Ryan E <81211706+comradeflats@users.noreply.github.com>
2023-02-06 21:04:52 +0700

    Update inandout.py

--- inandout.py
+++ inandout.py
@@ -3,7 +3,7 @@ import time
 import re
 
 # Discord webhook URL
-WEBHOOK_URL = 'your-discord-webhook-url'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1070608561568231498/bnodotPWDcGiv_3q-i1lcUoQD9WJ5z0e-h9GOVnpLFj05kHVO3E2ztMGY-nkOX7vi8Sx'
 
 # Dictionary to store the events that have already been sent to Discord
 sent_events = {}
@@ -16,11 +16,11 @@ def send_message(player, log_status, time_of_event):
 
 # Function that reads the Minecraft server logs
 def read_logs():
-    with open('/path/to/log/directory', 'r') as log_file:
+    with open('/opt/forge/logs/latest.log', 'r') as log_file:
         for line in log_file:
             if 'logged in' in line:
                 try:
-                    player = line.split(" ")[5]
+                    player = line.split(" ")[5].split("[")[0]
                     time_of_event = line.split('[')[1].split(']')[0].split(' ')[1]
                 except IndexError:
                     print("Error: Could not parse player information from line")
@@ -30,9 +30,9 @@ def read_logs():
                 if event not in sent_events:
                     sent_events[event] = True
                     send_message(player, "logged in to", time_of_event)
-            elif 'Disconnected' in line or 'left the game' in line:                
+            elif 'left the game' in line:
                 try:
-                    player = line.split(" ")[5]
+                    player = line.split(" ")[5].split("[")[0]
                     time_of_event = line.split('[')[1].split(']')[0].split(' ')[1]
                 except IndexError:
                     print("Error: Could not parse player information from line")
@@ -41,8 +41,8 @@ def read_logs():
                 event = f'{player} lost connection at {time_of_event}'
                 if event not in sent_events:
                     sent_events[event] = True
-                    send_message(player, "lost connection to", time_of_event)
+                    send_message(player, "lost connection from", time_of_event)
 
 while True:
     read_logs()
-    time.sleep(10)
+        time.sleep(10)
